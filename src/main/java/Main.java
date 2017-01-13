f38d6f7d853ed163c79ac60c3e2d338d1999371a
Ylia M <ysmorid@users.noreply.github.com>
2017-01-13 15:39:27 -0600

    Calls designBoard() method to display board when program starts,:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
+import java.io.PrintStream;
+
 /**
  * Created by ymoridza on 1/13/17.
  */
-public class main {
+public class Main {
+
+    public static void main(String[] args) {
+        PrintStream printStream = new PrintStream(System.out);
+        Board board = new Board(printStream);
+
+        board.designBoard();
+
+    }
 }
