4b15f97826b6cdc586a323ac4e80a245d4268bce
Ylia M <ysmorid@users.noreply.github.com>
2017-01-13 15:37:37 -0600

    created designBoard() to display game board.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -4,11 +4,18 @@ import java.io.PrintStream;
  * Created by ymoridza on 1/13/17.
  */
 public class Board {
+    private PrintStream printStream;
+
     public Board(PrintStream printStream) {
 
+        this.printStream = printStream;
     }
 
     public void designBoard(){
-
+        printStream.println("1 | 2 | 3");
+        printStream.println("- - - - -");
+        printStream.println("4 | 5 | 6");
+        printStream.println("- - - - -");
+        printStream.println("7 | 8 | 9");
     }
 }
