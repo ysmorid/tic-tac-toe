01de71d0c53f5e4578b33c2179d023d9e5787e6c
Ylia M <ysmorid@users.noreply.github.com>
2017-01-13 15:21:11 -0600

    created empty methods in order to set up test

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,7 +1,14 @@
-package PACKAGE_NAME;
+import java.io.PrintStream;
 
 /**
  * Created by ymoridza on 1/13/17.
  */
 public class Board {
+    public Board(PrintStream printStream) {
+
+    }
+
+    public void designBoard(){
+
+    }
 }
